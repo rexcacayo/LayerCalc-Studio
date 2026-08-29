ba22dad1a7d561d15a1bca447f11d0324a2015f7
Ricardo Lugaresi <rflugaresir@gmail.com>
2026-08-29 01:01:58 +0100

    fix: correct keyboard type for text fields

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 26
         targetSdk = 37
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
